8471a7de20e9043298b44fec0ebe41ae2f57d995
newTypeo <sinjh6348@naver.com>
2023-07-09 14:46:58 +0900

    movie.vo 클래스들 생성자 및 getter/setter 생성

--- src/main/java/com/cortles/project/movie/model/vo/Poster.java
+++ src/main/java/com/cortles/project/movie/model/vo/Poster.java
@@ -6,4 +6,43 @@ public class Poster {
 	private String posterId;
 	private String movieCode;
 	private Blob posterImg;
+	
+	public Poster() {}
+
+	public Poster(String posterId, String movieCode, Blob posterImg) {
+		super();
+		this.posterId = posterId;
+		this.movieCode = movieCode;
+		this.posterImg = posterImg;
+	}
+
+	public String getPosterId() {
+		return posterId;
+	}
+
+	public void setPosterId(String posterId) {
+		this.posterId = posterId;
+	}
+
+	public String getMovieCode() {
+		return movieCode;
+	}
+
+	public void setMovieCode(String movieCode) {
+		this.movieCode = movieCode;
+	}
+
+	public Blob getPosterImg() {
+		return posterImg;
+	}
+
+	public void setPosterImg(Blob posterImg) {
+		this.posterImg = posterImg;
+	}
+
+	@Override
+	public String toString() {
+		return "Poster [posterId=" + posterId + ", movieCode=" + movieCode + ", posterImg=" + posterImg + "]";
+	}
+	
 }
