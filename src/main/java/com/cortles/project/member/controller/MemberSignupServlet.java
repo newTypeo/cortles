7376e84aa91b76ab0abf1ecc4d5eb437b8d5d109
newTypeo <sinjh6348@naver.com>
2023-07-12 11:17:30 +0900

    유틸 암호화 추가

--- src/main/java/com/cortles/project/member/controller/MemberSignupServlet.java
+++ src/main/java/com/cortles/project/member/controller/MemberSignupServlet.java
@@ -1,25 +1,41 @@
 package com.cortles.project.member.controller;
 
 import java.io.IOException;
+import java.sql.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.cortles.project.common.util.CortlesUtils;
+import com.cortles.project.member.model.vo.Gender;
+
+
 @WebServlet("/member/memberSignup")
 public class MemberSignupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		System.out.println("서블릿 확인");
-		
-		
 		request.getRequestDispatcher("/WEB-INF/views/signup/signup.jsp")
 			.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String memberId = request.getParameter("memberId");
+		String password = CortlesUtils.getEncryptedPassword(request.getParameter("password"), memberId);
+		String name = request.getParameter("name");
+		String _gender = request.getParameter("gender");
+		String _birthday = request.getParameter("birthday");
+		String email = request.getParameter("email");
+		String phone = request.getParameter("phone");
+		String[] _genres = request.getParameterValues("genre");
+		
+		Gender gender = _gender != null ? Gender.valueOf(_gender) : null;
+		Date birthday = _birthday != null && !"".equals(_birthday) ? Date.valueOf(_birthday) : null;
+		String genre = _genres != null ? String.join(",", _genres) : null;
+		
 		
 	}
 
