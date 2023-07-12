e0274a7041b5998fe24cdbb654e950069d59e683
LeeJangJun1101 <wkdwnsdl123@naver.com>
2023-07-12 11:36:40 +0900

    이장준 보드작업

--- src/main/java/com/cortles/project/board/controller/BoardListServlet.java
+++ src/main/java/com/cortles/project/board/controller/BoardListServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.cortles.project.board.model.service.BoardService;
-import com.cortles.project.board.model.vo.Board;
+import com.cortles.project.board.model.vo.BoardEntity;
 
 /**
  * Servlet implementation class BoardServlet
@@ -25,6 +25,37 @@ public class BoardListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 사용자입력값 처리
+		int cpage = 1; // 기본값처리
+		try {
+			cpage = Integer.parseInt(request.getParameter("cpage")); 			
+		} catch (NumberFormatException e) {
+			// 예외처리외에 아무것도 하지 않음.
+		}
+		// cpage = 1 -> start = 1, end = 10
+		// cpage = 2 -> start = 11, end = 20
+		// cpage = 3 -> start = 21, end = 30
+		int start = (cpage - 1) * LIMIT + 1;
+		int end = cpage * LIMIT;
+		
+		// 2. 업무로직
+		List<BoardEntity> boards = boardService.findAll(start, end);
+		System.out.println("boards = " + boards);
+		
+		// xss공격대비처리
+		for(BoardEntity board : boards) {
+			board.setTitle(HelloMvcUtils.escapeHtml(board.getTitle()));
+		}
+		
+		// 페이지바영역 처리
+		int totalContent = boardService.getTotalContent();
+		System.out.println("totalContent = " + totalContent);
+		String url = request.getRequestURI(); // /mvc/board/boardList
+		String pagebar = HelloMvcUtils.getPagebar(cpage, LIMIT, totalContent, url);
+		System.out.println("pagebar = " + pagebar);
+		
+		request.setAttribute("boards", boards);
+		request.setAttribute("pagebar", pagebar);
 		
 		// 3. 응답처리
 		request.getRequestDispatcher("/WEB-INF/views/board/board.jsp").forward(request, response);
--- src/main/java/com/cortles/project/board/model/service/BoardService.java
+++ src/main/java/com/cortles/project/board/model/service/BoardService.java
@@ -1,8 +1,16 @@
 package com.cortles.project.board.model.service;
 
+import java.util.List;
+
 import com.cortles.project.board.model.dao.BoardDao;
+import com.cortles.project.board.model.vo.BoardEntity;
 
 public class BoardService {
 	private final BoardDao boardDao = new BoardDao();
+
+	public List<BoardEntity> findAll(int start, int end) {
+		
+		return null;
+	}
 
 }
