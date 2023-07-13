051f1f024457cf22f8304c4847b920874bb9029a
LeeJangJun1101 <wkdwnsdl123@naver.com>
2023-07-13 19:15:36 +0900

    Merge branch 'master' of https://github.com/newTypeo/cortles.git

---
+++ src/main/java/com/cortles/project/admin/model/exception/AdminException.java
@@ -0,0 +1,29 @@
+package com.cortles.project.admin.model.exception;
+
+public class AdminException extends RuntimeException {
+
+	public AdminException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdminException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdminException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdminException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdminException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+}
