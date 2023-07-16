177619139099c80453c6200702d1b88fb238eb20
ijangjun <ijangjun@localhost>
2023-07-16 18:11:08 +0900

    게시판 댓글 찢어발

--- src/main/java/com/cortles/project/board/controller/BoardCreateServlet.java
+++ src/main/java/com/cortles/project/board/controller/BoardCreateServlet.java
@@ -18,6 +18,7 @@ import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 import com.oreilly.servlet.multipart.FileRenamePolicy;
 
+
 /**
  * Servlet implementation class BoardCreateServlet
  */
@@ -53,6 +54,7 @@ public class BoardCreateServlet extends HttpServlet {
 			// 파일명 재지정 정책객체
 			// 한글.txt --> 20230629_160430123_999.txt
 			FileRenamePolicy policy = new DefaultFileRenamePolicy();
+			  
 			
 			MultipartRequest multiReq = new MultipartRequest(request, saveDirectory, maxPostSize, encoding, policy);
 			
