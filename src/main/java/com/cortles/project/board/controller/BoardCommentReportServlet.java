3d7f3a2c1a8f27c6d12ef6d9ba4a77dc04763e85
LeeJangJun1101 <wkdwnsdl123@naver.com>
2023-07-17 19:33:15 +0900

    sfa

--- src/main/java/com/cortles/project/board/controller/BoardCommentReportServlet.java
+++ src/main/java/com/cortles/project/board/controller/BoardCommentReportServlet.java
@@ -32,7 +32,7 @@ public class BoardCommentReportServlet extends HttpServlet {
 		 String reportContent = request.getParameter("reportContent");
 		 String reportedId = request.getParameter("reportedId");
 		 
-		 ReportComment reportComment = new ReportComment(commentNo, boardNo, 0, reporterId, reportedId,reportContent, null, reportType, null);
+		 ReportComment reportComment = new ReportComment(commentNo, boardNo, 0, reporterId, reportedId,null, reportContent, reportType, null);
 		 
 		 int result = boardService.insertReportBoardComment(reportComment);
 		 
